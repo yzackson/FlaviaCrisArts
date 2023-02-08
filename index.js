d37e8312686af96f9283b35b4cf851dd4a9342c0
Isaac Pires <isaac.aragao.p@gmail.com>
2023-02-08 09:53:36 -0300

    engines in package.json

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get('/', (request, response) => {
     response.sendFile(`${appRoot.path}\\pages\\index.html`)
 });
 
-var port = server.listen(process.env.PORT || 3000);
 
-app.listen(port, () => console.log("Servidor UP!"));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
