d3e4d7771605395a24570ba771deb61ee8aa5cf5
Isaac Pires <isaac.aragao.p@gmail.com>
2023-02-08 09:26:21 -0300

    change root path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const appRoot = require('app-root-path');
 app.use(express.static(appRoot.path));
 
 app.get('/', (request, response) => {
-    response.sendFile(`C:\\Dev\\PersonalProjects\\FlaviaCrisArtsSite\\pages\\index.html`)
+    response.sendFile(`${appRoot.path}\\pages\\index.html`)
 });
 
 app.listen(3300, () => console.log("Servidor UP!"));
